19125137e308fa050d197bd30366e62eff0698ac
Viliam Simko <viliam.simko@gmail.com>
2018-06-04 17:00:13 +0200

    use asyn-await for promises

--- mqtt-demo-eventlogger/index.js
+++ mqtt-demo-eventlogger/index.js
@@ -6,22 +6,17 @@ const client = mqtt.connect('mqtt://localhost:1883/')
 const axios = require('axios')
 const url = "http://localhost:4000"
 
-client.on('message', (topicname, message) => {
+client.on('message', async (topicname, message) => {
     const jsonmsg = JSON.parse(message.toString())
     console.log(`got mqtt message from topic: ${topicname}`)
-    let data = {"query":`mutation {solrInsert(topic:\"${topicname}\",data:\"${jsonmsg.rndnum}\")}`}
 
-    axios.post(url, data)
-.then((response) => {
-        console.log(response)
-    })
-        .catch((error)=>{
-            console.log(error)
-        })
-    const msg = JSON.stringify(jsonmsg)
-    console.log(msg)
+    const data = {
+        "query": `mutation {solrInsert(topic:"${topicname}",data:"${jsonmsg.rndnum}\")}`
+    }
+
+    await axios.post(url, data)
+    console.log("sent:" + JSON.stringify(data))
 })
-
 
 client.on('connect', function () {
     console.log("connected to mqtt, now sending messages")
